bf89730e4ba7aa5158ebc03d245ebdbb2ba72474
AnibalCardozo <anibal@sappitotech.com>
2019-10-03 10:01:30 -0400

    adds generic response from api

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -18,7 +18,7 @@ export class FormComponent implements OnInit {
   onCheckAvailability(){
     if(this.buttonLabel === 'GET Pokimen'){
       let pokeId = Math.floor(Math.random() * 151); 
-      this.http.get(`https://pokeapi.co/api/v2/pokemon/${pokeId}`).subscribe(pokemon =>{
+      this.http.get(`https://pokeapi.co/api/v2/pokemon/${pokeId}`).subscribe((pokemon: any) =>{
         alert(pokemon.name);
       })
     }
